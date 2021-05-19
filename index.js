30cc6976580658a280f47edfae0c754ab6eda607
Lovecoding <lqingsong7@gmail.com>
2021-05-19 10:57:54 -0700

    setting up mongodb

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./db');
+
+//Routers
+const userRouter = require('./routes/user-router');
 
 const app = express();
 
 const apiPort = 1337;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
    res.send('Hello world!');
 });
+
+app.use('/api', [
+    userRouter
+]);
 
 app.listen(apiPort, () => {
     console.log('Express server listening on port ' + apiPort);
---
+++ routes/user-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const UserController = require('../controllers/user-controller');
+
+const router = express.Router();
+
+module.exports = router;
