27c552a374525c54df5733fae31352a69e7a5744
Lovecoding <lqingsong7@gmail.com>
2021-05-19 15:15:56 -0700

    Login function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,10 @@
 const User = require("../db/models/user-model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../db/secretKey");
 
 const validateRegisterInput = require("../validations/register");
+const validateLoginInput = require("../validations/login");
 
 const createUser = async (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -32,6 +35,56 @@ const createUser = async (req, res) => {
     }
 };
 
+const loginUser = (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { email, password } = req.body;
+
+    User.findOne({ email }).then((user) => {
+        if (!user) {
+            return res.status(404).json({ emailNotFound: "Email not found" });
+        }
+
+        bcrypt.compare(password, user.password).then((isMatch) => {
+            if (isMatch) {
+                const payload = {
+                    _id: user.id,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                    email: user.email,
+                    status: user.status,
+                    avatar: user.avatar,
+                    role: user.role
+                };
+
+                jwt.sign(
+                    payload,
+                    secretKey,
+                    {
+                        expiresIn: 31556926, // 1 year in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token,
+                            data: payload,
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({ passwordIncorrect: "Password Incorrect" });
+            }
+        });
+    })
+};
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
 };
--- routes/user-router.js
+++ routes/user-router.js
@@ -5,5 +5,6 @@ const UserController = require('../controllers/user-controller');
 const router = express.Router();
 
 router.post("/register", UserController.createUser);
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
